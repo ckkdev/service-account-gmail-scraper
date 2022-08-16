7a4da92f8a4b40b094610190d7743060cc051997
ckkdev <99091096+ckkdev@users.noreply.github.com>
2022-08-16 15:04:56 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from scraper import AccountServiceScraper
 
 SCOPES = ['https://mail.google.com/']
 SERVICE_ACCOUNT_FILE = 'keyfile.json'
-EMAIL_USERS = ['genero@ckk.dev', 'dustin@ckk.dev']
+EMAIL_USERS = ['email1@domain.com', 'email2@domain.com', 'email3@domain.com']
 
 AccountServiceScraper(EMAIL_USERS, SCOPES, SERVICE_ACCOUNT_FILE)
